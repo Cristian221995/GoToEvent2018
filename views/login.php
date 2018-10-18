a52603f6648c649acea905ffa697c61d29b61dae
DMasudd <DMasudd@users.noreply.github.com>
2018-10-18 11:18:43 -0300

    view login

--- views/login.php
+++ views/login.php
@@ -6,10 +6,10 @@
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
         <link rel="stylesheet" href="css/style.css">
     </head>
-    <body class="d-flex align-items-center justify-content-center">
+    <body class="d-flex align-items-center justify-content-center" background = "morado.jpg">
 
 
-        <form id="login-form" class="m-2" action="Artist/store" method="POST">
+        <form id="login-form" class="m-2" action="Artist/index" method="POST">
             <div class="text-center mb-4">
                 <strong>Login</strong>
             </div>
@@ -32,7 +32,7 @@
                     <input type="password" name="pass" value="" class="form-control">
                 </div>
             </fieldset>
-            <button type="submit" name="button" class="btn btn-primary btn-block">Crear artista</button>
+            <button type="submit" name="button" class="btn btn-primary btn-block">Iniciar Sesion</button>
         </form>
 
 
