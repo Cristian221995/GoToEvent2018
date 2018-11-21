b77374e18f2e63573f635bae09a78c885501d5d5
Cristian221995 <Cristian221995@users.noreply.github.com>
2018-11-21 20:32:47 -0300

    Clases faltantes

---
+++ models/ticket.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace models;
+
+class Ticket
+{
+    private $number;
+    private $qrCode;
+
+    public function __construct($number, $qrCode)
+    {
+            $this->number=$number;
+            $this->qrCode=$qrCode;
+    }
+
+    public function getNumber(){
+        return $this->number;
+    }
+
+    public function setNumber($number){
+        $this->number=$number;
+    }
+
+    public function getQrCode(){
+        return $this->qrCode;
+    }
+
+    public function setQrCode($qrCode){
+        $this->qrCode=$qrCode;
+    }
+}
+?>
